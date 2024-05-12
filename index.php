a486c0e687f3ff95ff8047712539350f0035d791
trhacknon <129875733+tucommenceapousser@users.noreply.github.com>
2024-05-12 02:30:07 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,234 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta name="description" content="Anonymous hacktivists hackers !">
+      <meta property="og:title" content="Anonymous">
+      <meta property="og:description" content="Anonymous !">
+      <meta property="og:image" content="anonn.svg">
+      <meta property="og:url" content="https://c06b2c03-36d9-4f09-82cd-cd2edb7c0656-00-117ntbo0mk0od.picard.replit.dev/">
+      <meta name="twitter:card" content="summary_large_image">
+      <meta name="twitter:title" content="Anonymous">
+      <meta name="twitter:description" content="Anonymous !">
+      <meta name="twitter:image" content="anonn.svg">
+      <link rel="stylesheet" href="sty.css">
+      <link rel="preload" href="anonn.svg" as="image">
+      <link rel="icon" href="favicon.ico" type="image/x-icon">
+<title>Who are anonymous</title>
+<style>
+    body {
+        background-color: #000;
+        color: #0f0; /* Vert fluo */
+        font-family: 'Courier New', monospace;
+        padding: 50px;
+    }
+    h1 {
+        color: #0ff; /* Cyan fluo */
+        text-align: center;
+        font-size: 28px; /* Réduire la taille de la police */
+        text-shadow: 2px 2px 4px #00f;
+    }
+    h2 {
+        color: #ff0; /* Jaune fluo */
+        text-shadow: 2px 2px 4px #f00;
+    }
+    p {
+        color: #f0f; /* Magenta fluo */
+    }
+    .section {
+        margin-bottom: 30px;
+    }
+    /* Animation arc-en-ciel */
+    @keyframes rainbow {
+        0% { color: red; }
+        16.666% { color: orange; }
+        33.333% { color: yellow; }
+        50% { color: green; }
+        66.666% { color: blue; }
+        83.333% { color: indigo; }
+        100% { color: violet; }
+    }
+    .rainbow-text {
+        animation: rainbow 4s linear infinite;
+    }
+    @keyframes rainboww {
+        0% { color: green; }
+        16.666% { color: blue; }
+        33.333% { color: violet; }
+        50% { color: indigo; }
+        66.666% { color: orange; }
+        83.333% { color: yellow; }
+        100% { color: red; }
+    }
+    .rainboww-text {
+        animation: rainboww 4s linear infinite;
+    }
+    .ascii {
+        margin-top: 20px;
+        text-align: center;
+        text-shadow: 2px 2px 4px #f00;
+    }
+    .logo {
+        width: 100%; /* Vous pouvez ajuster cette valeur selon la taille désirée */
+        margin: 0 auto; /* Cela centrer le logo horizontalement */
+        border: 2px solid lightcoral;
+        padding: 10px;
+        margin-bottom: 10px;
+        background-color: #222;
+    }
+    .language {
+        text-align: center;
+        margin-bottom: 20px;
+    }
+    .language a {
+        margin-right: 10px;
+        color: #0ff;
+        text-decoration: none;
+    }
+    .language a:hover {
+        text-decoration: underline;
+    }
+</style>
+</head>
+<body>
+      <img src="anonn.svg" alt="TRHACKNON logo" class="logo">
+      <div class="language">
+            <a href="?lang=fr">Français</a>
+            <a href="?lang=en">English</a>
+      </div>
+<h1>Who are anonymous, hacktivists, black blocks, whistleblowers, Hackers red hat <span class="rainbow-text">by TRHACKNON</span></h1>
+
+    <div class="section">
+        <?php if(!isset($_GET['lang']) || (isset($_GET['lang']) && $_GET['lang'] !== 'en')) { ?>
+            <h2>Anonymous</h2>
+            <p>Les Anonymous sont un groupe international de hackers et d'activistes en ligne qui se mobilisent pour diverses causes, souvent en menant des opérations de piratage informatique et en lançant des attaques par déni de service distribué (DDoS).</p>
+        <?php } ?>
+    </div>
+
+    <div class="ascii rainboww-text">
+        <?php if(!isset($_GET['lang']) || (isset($_GET['lang']) && $_GET['lang'] !== 'en')) { ?>
+            <pre>( `_•́)︻デ═一╭∩╮( •̀_•́ )╭∩╮ ╾━╤デ╦︻(•̀⤙•́)</pre>
+        <?php } ?>
+    </div>
+
+    <div class="section">
+        <?php if(!isset($_GET['lang']) || (isset($_GET['lang']) && $_GET['lang'] !== 'en')) { ?>
+            <h2>Hacktivists</h2>
+            <p>Les hacktivistes sont des activistes qui utilisent les compétences en informatique et le piratage éthique pour promouvoir des changements sociaux ou politiques. Ils peuvent mener des attaques informatiques ciblées ou des campagnes de sensibilisation en ligne.</p>
+        <?php } ?>
+    </div>
+
+    <div class="ascii rainbow-text">
+        <?php if(!isset($_GET['lang']) || (isset($_GET['lang']) && $_GET['lang'] !== 'en')) { ?>
+            <pre>( `_•́)︻デ═一╭∩╮( •̀_•́ )╭∩╮ ╾━╤デ╦︻(•̀⤙•́)</pre>
+        <?php } ?>
+    </div>
+
+    <div class="section">
+        <?php if(!isset($_GET['lang']) || (isset($_GET['lang']) && $_GET['lang'] !== 'en')) { ?>
+            <h2>Lanceurs d'alertes</h2>
+            <p>Les lanceurs d'alertes sont des individus qui divulguent des informations confidentielles ou sensibles dans l'intérêt public. Ils peuvent dénoncer des actes répréhensibles, des violations des droits de l'homme ou des pratiques illégales au sein d'entreprises ou de gouvernements.</p>
+        <?php } ?>
+    </div>
+
+    <div class="ascii rainboww-text">
+        <?php if(!isset($_GET['lang']) || (isset($_GET['lang']) && $_GET['lang'] !== 'en')) { ?>
+            <pre>( `_•́)︻デ═一╭∩╮( •̀_•́ )╭∩╮ ╾━╤デ╦︻(•̀⤙•́)</pre>
+        <?php } ?>
+    </div>
+
+    <div class="section">
+        <?php if(!isset($_GET['lang']) || (isset($_GET['lang']) && $_GET['lang'] !== 'en')) { ?>
+            <h2>Hackers Red Hat</h2>
+            <p>Les hackers Red Hat sont des pirates informatiques éthiques qui utilisent leurs compétences en sécurité informatique pour protéger les systèmes et les réseaux contre les attaques. Ils peuvent effectuer des tests de pénétration, identifier les vulnérabilités et proposer des solutions pour renforcer la sécurité.</p>
+        <?php } ?>
+    </div>
+
+    <div class="ascii rainbow-text">
+        <?php if(!isset($_GET['lang']) || (isset($_GET['lang']) && $_GET['lang'] !== 'en')) { ?>
+            <pre>( `_•́)︻デ═一╭∩╮( •̀_•́ )╭∩╮ ╾━╤デ╦︻(•̀⤙•́)</pre>
+        <?php } ?>
+    </div>
+            <div class="section">
+                <?php if(!isset($_GET['lang']) || (isset($_GET['lang']) && $_GET['lang'] !== 'en')) { ?>
+                <h2>Proposer une cible</h2>
+                <p>Vous pouvez proposer une cible qui correspond aux principes défendus par Anonymous, les anarchistes, les hacktivistes, etc., à des fins humanitaires ou altruistes. Votre contribution est appréciée.</p>
+                <form action="submit_target.php" method="post">
+                    <label for="target_name">Nom de la cible:</label><br>
+                    <input type="text" id="target_name" name="target_name" required><br>
+                    <label for="target_description">Description:</label><br>
+                    <textarea id="target_description" name="target_description" rows="4" required></textarea><br>
+                    <input type="submit" value="Proposer">
+                </form>
+            </div>
+            <div class="ascii rainbow-text">
+                <pre>( `_•́)︻デ═一╭∩╮( •̀_•́ )╭∩╮ ╾━╤デ╦︻(•̀⤙•́)</pre>
+                <?php } ?>
+            </div>
+    <?php
+        // Vérifier si la langue est définie sur l'anglais
+        if(isset($_GET['lang']) && $_GET['lang'] === 'en') {
+    ?>
+            <div class="section">
+                <h2>Anonymous</h2>
+                <p>Anonymous is an international group of hackers and online activists who mobilize for various causes, often engaging in hacking operations and launching distributed denial-of-service (DDoS) attacks.</p>
+            </div>
+
+            <div class="ascii rainboww-text">
+                <pre>( `_•́)︻デ═一╭∩╮( •̀_•́ )╭∩╮ ╾━╤デ╦︻(•̀⤙•́)</pre>
+            </div>
+
+            <div class="section">
+                <h2>Hacktivists</h2>
+                <p>Hacktivists are activists who use computer skills and ethical hacking to promote social or political changes. They may conduct targeted cyber attacks or online awareness campaigns.</p>
+            </div>
+
+            <div class="ascii rainbow-text">
+                <pre>( `_•́)︻デ═一╭∩╮( •̀_•́ )╭∩╮ ╾━╤デ╦︻(•̀⤙•́)</pre>
+            </div>
+
+            <div class="section">
+                <h2>Whistleblowers</h2>
+                <p>Whistleblowers are individuals who disclose confidential or sensitive information in the public interest. They may expose wrongdoing, human rights violations, or illegal practices within companies or governments.</p>
+            </div>
+
+            <div class="ascii rainboww-text">
+                <pre>( `_•́)︻デ═一╭∩╮( •̀_•́ )╭∩╮ ╾━╤デ╦︻(•̀⤙•́)</pre>
+            </div>
+
+            <div class="section">
+                <h2>Red Hat Hackers</h2>
+                <p>Red Hat hackers are ethical computer hackers who use their cybersecurity skills to protect systems and networks against attacks. They may perform penetration testing, identify vulnerabilities, and propose solutions to strengthen IT infrastructure security.</p>
+            </div>
+
+            <div class="ascii rainbow-text">
+                <pre>( `_•́)︻デ═一╭∩╮( •̀_•́ )╭∩╮ ╾━╤デ╦︻(•̀⤙•́)</pre>
+            </div>
+
+            <div class="section">
+                <h2>Black Bloc</h2>
+                <p>Black Blocs are autonomous and anonymous groups that participate in demonstrations and direct actions. They often wear black clothing and masks to conceal their identity and protect their anonymity during protests.</p>
+            </div>
+            <div class="ascii rainbow-text">
+                <pre>( `_•́)︻デ═一╭∩╮( •̀_•́ )╭∩╮ ╾━╤デ╦︻(•̀⤙•́)</pre>
+            </div>
+            <div class="section">
+                <h2>Propose a Target</h2>
+                <p>You can propose a target that aligns with the principles advocated by Anonymous, anarchists, hacktivists, etc., for humanitarian or altruistic purposes. Your contribution is appreciated.</p>
+                <form action="submit_target.php" method="post">
+                    <label for="target_name">Target Name:</label><br>
+                    <input type="text" id="target_name" name="target_name" required><br>
+                    <label for="target_description">Description:</label><br>
+                    <textarea id="target_description" name="target_description" rows="4" required></textarea><br>
+                    <input type="submit" value="Propose">
+                </form>
+            </div>
+            <div class="ascii rainbow-text">
+                <pre>( `_•́)︻デ═一╭∩╮( •̀_•́ )╭∩╮ ╾━╤デ╦︻(•̀⤙•́)</pre>
+            </div>
+            <?php
+                }
+            ?>    
+</body>
+</html>
